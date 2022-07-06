2611b841a0ad708c9bbd22e0ce302bb2ec95466d
chubbybaby31 <49795066+chubbybaby31@users.noreply.github.com>
2022-07-06 12:54:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 import os
-
-print("\t\t    +------------------+")
-print("\t\t    |   SwitchX v1.0   |")
-print("\t\t    +------------------+")
-print("\t\t    (c) Aodrulez\n")
-print("( Adds support for XBOX360 controllers to Nintendo Switch )\n")
 if not os.geteuid() == 0:
 	raise PermissionError(' [-] Script must be run as root/sudo!')
 print("[+] Installing required packages/libraries & setting up everything!\n\n")
 os.system("apt-get -o Acquire::ForceIPv4=true update")
 os.system("apt-get -o Acquire::ForceIPv4=true install -y python3 python3-pip xboxdrv python3-dbus supervisor")
 os.system("pip3 install hid aioconsole dbus-python")
+os.system("pip3 install hid==1.0.0")
+os.system("pip3 install inputs")
 os.system('echo "[program:switchX]" > /etc/supervisor/conf.d/switchX.conf')
 os.system('echo "priority=10" >> /etc/supervisor/conf.d/switchX.conf')
 os.system('echo "directory='+os.getcwd()+'" >> /etc/supervisor/conf.d/switchX.conf')
@@ -19,4 +15,3 @@ os.system('echo "user=root" >> /etc/supervisor/conf.d/switchX.conf')
 os.system('echo "autostart=true" >> /etc/supervisor/conf.d/switchX.conf')
 os.system('echo "autorestart=true" >> /etc/supervisor/conf.d/switchX.conf')
 print("[+] Done.")
-print("[+] Reboot now!")
